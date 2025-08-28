feaff5d80c843d83cc74066f757cd3b9bde1157a
alessio176 <alessio176@gmail.com>
2025-08-28 13:18:18 +0200

    detail panel GUI form

--- src/main/java/it/cri/demo/ui/VolunteerDetails.java
+++ src/main/java/it/cri/demo/ui/VolunteerDetails.java
@@ -4,6 +4,32 @@ import javax.swing.*;
 
 public class VolunteerDetails {
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton textField2;
+    private JTextField nameField;
+    private JTextField surnameField;
+    private JTextField birthDateField;
+    private JTextField birthPlaceField;
+    private JTextField phoneField;
+    private JTextField emailField;
+    private JTextField residenceField;
+    private JTextField registrationNumberField;
+    private JTextField feeField;
+    private JTextField rankField;
+    private JTextField lastRecallField;
+    private JTextField activityField;
+    private JTextField lastVisitField;
+    private JTextField nextVisitField;
+    private JLabel nameLabel;
+    private JLabel surnameLabel;
+    private JLabel birthDateLabel;
+    private JLabel birthPlaceLabel;
+    private JLabel phoneLabel;
+    private JLabel emailLabel;
+    private JLabel residenceLabel;
+    private JLabel registrationNumberLabel;
+    private JLabel feeLabel;
+    private JLabel rankLabel;
+    private JLabel lastRecallLabel;
+    private JLabel activityLabel;
+    private JLabel lastVisitLabel;
+    private JLabel nextVisitLabel;
 }
