adc19f82a95ebb36fbc527f307442319dd04a4b4
alessio176 <alessio176@gmail.com>
2025-08-28 15:17:06 +0200

    fix on gui startup and intelliJ config

--- src/main/java/it/cri/demo/entity/Volunteer.java
+++ src/main/java/it/cri/demo/entity/Volunteer.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Entity
-@Data
+//@Data
 @Table
 public class Volunteer {
 
@@ -33,4 +33,136 @@ public class Volunteer {
     //Quota associativa
     private Double fee;
 
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getResidenceAddress() {
+        return residenceAddress;
+    }
+
+    public void setResidenceAddress(String residenceAddress) {
+        this.residenceAddress = residenceAddress;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getRankValue() {
+        return rankValue;
+    }
+
+    public void setRankValue(String rankValue) {
+        this.rankValue = rankValue;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPecAddress() {
+        return pecAddress;
+    }
+
+    public void setPecAddress(String pecAddress) {
+        this.pecAddress = pecAddress;
+    }
+
+    public LocalDate getLastRecall() {
+        return lastRecall;
+    }
+
+    public void setLastRecall(LocalDate lastRecall) {
+        this.lastRecall = lastRecall;
+    }
+
+    public String getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(String lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+    public LocalDate getLastMedicalVisit() {
+        return lastMedicalVisit;
+    }
+
+    public void setLastMedicalVisit(LocalDate lastMedicalVisit) {
+        this.lastMedicalVisit = lastMedicalVisit;
+    }
+
+    public LocalDate getNextMedicalVisit() {
+        return nextMedicalVisit;
+    }
+
+    public void setNextMedicalVisit(LocalDate nextMedicalVisit) {
+        this.nextMedicalVisit = nextMedicalVisit;
+    }
+
+    public Double getFee() {
+        return fee;
+    }
+
+    public void setFee(Double fee) {
+        this.fee = fee;
+    }
 }
