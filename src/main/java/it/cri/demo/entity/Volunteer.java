ada74e0327866321ec10772d54fa7b25fa85d1ad
alessio176 <alessio176@gmail.com>
2025-09-06 00:32:21 +0200

    lombok back in Volunteer

--- src/main/java/it/cri/demo/entity/Volunteer.java
+++ src/main/java/it/cri/demo/entity/Volunteer.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-//@Data
+@Data
 @Table
 public class Volunteer {
 
@@ -25,7 +25,7 @@ public class Volunteer {
     //grado
     private String rankValue;
     private String phoneNumber;
-    private String  emailAddress;
+    private String emailAddress;
     private String pecAddress;
     private LocalDate lastRecall;
     private String lastActivity;
@@ -57,132 +57,5 @@ public class Volunteer {
     public String toString() {
         return rankValue + " " + name + " " + surname;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String getResidenceAddress() {
-        return residenceAddress;
-    }
-
-    public void setResidenceAddress(String residenceAddress) {
-        this.residenceAddress = residenceAddress;
-    }
-
-    public String getBirthPlace() {
-        return birthPlace;
-    }
-
-    public void setBirthPlace(String birthPlace) {
-        this.birthPlace = birthPlace;
-    }
-
-    public String getRankValue() {
-        return rankValue;
-    }
-
-    public void setRankValue(String rankValue) {
-        this.rankValue = rankValue;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getPecAddress() {
-        return pecAddress;
-    }
-
-    public void setPecAddress(String pecAddress) {
-        this.pecAddress = pecAddress;
-    }
-
-    public LocalDate getLastRecall() {
-        return lastRecall;
-    }
-
-    public void setLastRecall(LocalDate lastRecall) {
-        this.lastRecall = lastRecall;
-    }
-
-    public String getLastActivity() {
-        return lastActivity;
-    }
-
-    public void setLastActivity(String lastActivity) {
-        this.lastActivity = lastActivity;
-    }
-
-    public LocalDate getLastMedicalVisit() {
-        return lastMedicalVisit;
-    }
-
-    public void setLastMedicalVisit(LocalDate lastMedicalVisit) {
-        this.lastMedicalVisit = lastMedicalVisit;
-    }
-
-    public LocalDate getNextMedicalVisit() {
-        return nextMedicalVisit;
-    }
-
-    public void setNextMedicalVisit(LocalDate nextMedicalVisit) {
-        this.nextMedicalVisit = nextMedicalVisit;
-    }
-
-    public Double getFee() {
-        return fee;
-    }
-
-    public void setFee(Double fee) {
-        this.fee = fee;
-    }
 }
+
